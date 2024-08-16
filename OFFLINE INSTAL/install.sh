cf40fd6782d05802236191bb69bccacdb4aac00d
pvzchekmeneva20 <132128775+pvzchekmeneva20@users.noreply.github.com>
2024-08-16 15:03:06 +0300

    Update install.sh

--- OFFLINE INSTAL/install.sh
+++ OFFLINE INSTAL/install.sh
@@ -2,8 +2,12 @@
 
 # Путь к папке назначения для скрипта run.sh
 DEST_SCRIPT="$HOME/run.sh"
+
+# Автоматическое определение пути к рабочему столу
+DESKTOP_DIR=$(xdg-user-dir DESKTOP)
+
 # Путь к дескриптору на рабочем столе
-DEST_DESKTOP="$HOME/Рабочий стол/npos-api-control.desktop"
+DEST_DESKTOP="$DESKTOP_DIR/npos-api-control.desktop"
 
 # Создаём папки и копируем файлы
 echo "Копируем run.sh в $DEST_SCRIPT..."
